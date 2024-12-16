93f0a6a1de9705da834b39cdbefc8400761bebde
adnanmahmutovic <mahmutovic2025@gmail.com>
2024-12-15 22:20:06 -0600

    new test

--- xml_reader.cpp
+++ xml_reader.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
         cout << "Error loading XML file!" << endl;
         return 1;
     }
-    //test comment
+    //test
 
     XMLElement* catalog = doc.FirstChildElement("catalog");
     if (catalog == nullptr) {
